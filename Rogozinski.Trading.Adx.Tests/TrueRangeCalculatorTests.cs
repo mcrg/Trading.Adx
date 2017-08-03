f9fa3ea105c1e6fd68a00d09ebb16e35ad5e6d16
Maciej Rogozinski <maciej@rogozinski.net>
2017-08-03 22:01:07 +0200

    Implement True Range calculator

---
+++ Rogozinski.Trading.Adx.Tests/TrueRangeCalculatorTests.cs
@@ -0,0 +1,43 @@
+﻿namespace Rogozinski.Trading.Adx.Tests
+{
+    using Xunit;
+
+    public class TrueRangeCalculatorTests
+    {
+        [Fact]
+        public void CalculateShouldReturnDistanceBetweenCurrentHighAndLow()
+        {
+            var sut = new TrueRangeCalculator();
+            var previousPrice = new PricePoint(4, 8, 2, 6);
+            var currentPrice = new PricePoint(6, 12, 4, 10);
+
+            var actual = sut.Calculate(previousPrice, currentPrice);
+
+            Assert.Equal(8, actual);
+        }
+
+        [Fact]
+        public void CalculateShouldReturnDistanceBetweenCurrentHighAndPreviousClose()
+        {
+            var sut = new TrueRangeCalculator();
+            var previousPrice = new PricePoint(2, 6, 0, 4);
+            var currentPrice = new PricePoint(10, 16, 12, 14);
+
+            var actual = sut.Calculate(previousPrice, currentPrice);
+
+            Assert.Equal(12, actual);
+        }
+
+        [Fact]
+        public void CalculateShouldReturnDistanceBetweenCurrentLowAndPreviousClose()
+        {
+            var sut = new TrueRangeCalculator();
+            var previousPrice = new PricePoint(14, 16, 10, 12);
+            var currentPrice = new PricePoint(6, 8, 2, 4);
+
+            var actual = sut.Calculate(previousPrice, currentPrice);
+
+            Assert.Equal(10, actual);
+        }
+    }
+}
---
+++ Rogozinski.Trading.Adx/TrueRangeCalculator.cs
@@ -0,0 +1,17 @@
+﻿namespace Rogozinski.Trading.Adx
+{
+    using System;
+    using System.Linq;
+
+    public class TrueRangeCalculator
+    {
+        public double Calculate(IPricePoint previousPrice, IPricePoint currentPrice)
+        {
+            var currentHighToLow = Math.Abs(currentPrice.High - currentPrice.Low);
+            var currentHighToPreviousClose = Math.Abs(currentPrice.High - previousPrice.Close);
+            var currentLowToPreviousClose = Math.Abs(currentPrice.Low - previousPrice.Close);
+
+            return (new double[] { currentHighToLow, currentHighToPreviousClose, currentLowToPreviousClose }).Max();
+        }
+    }
+}
